bbe7a5978c000e3b719045298c1ea20a458db3af
Ivo Maixner <ivo.maixner@gmail.com>
2016-11-08 09:41:42 +0100

    Add clj-01.

---
+++ src/clojure_crumbs/clj_01.clj
@@ -0,0 +1,41 @@
+(ns clojure-crumbs.clj-01)
+
+; function call
+(+ 1 1)
+
+; functions are more flexible than operators
+(+ 1 2 3 4)
+(* 1 2 3 4)
+
+; (clojure.repl/doc +)
+; (doc +)
+
+; (source +)
+
+; ===========================
+; DATA TYPES: SCALAR LITERALS
+; ===========================
+
+; int
+42
+
+; rational
+22/7
+
+; approximate decimal
+3.14
+
+; char
+'A
+
+; string
+"Some nice string"
+
+; boolean
+true
+false
+
+; no value
+nil
+
+;
